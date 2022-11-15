e52b7067b2f9914e86acda7b0a7d0e0db594203c
Dontavious Green <dontavious.o.green@gmail.com>
2022-11-15 07:13:36 -0600

    init

---
+++ src/main/java/yorksolutions/net/capstoneeditorbe/CapstoneEditorBeApplication.java
@@ -0,0 +1,13 @@
+package yorksolutions.net.capstoneeditorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEditorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEditorBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yorksolutions/net/capstoneeditorbe/CapstoneEditorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package yorksolutions.net.capstoneeditorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneEditorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
